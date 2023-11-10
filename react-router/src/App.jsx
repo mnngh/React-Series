254c3ee585cfe70abb8bd7a2784078359294c7c4
mnn <mnn.saag@gmail.com>
2023-11-10 11:25:31 +0300

    first commit in react-router

---
+++ react-router/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className='text-4xl font-black justify-center items-center'>
+        Hello Nur
+      </h1>
+    </>
+  );
+}
+
+export default App;
