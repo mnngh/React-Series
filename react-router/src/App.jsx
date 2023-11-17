8c3b495027cf8b60483f9a2da3a5f4bb930bfac8
mnn <mnn.saag@gmail.com>
2023-11-17 13:12:01 +0300

    u

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <>
-      <h1 className='text-4xl font-black justify-center items-center'>
-        Hello Nur
-      </h1>
+      <Header />
+      <Home />
+      <Footer />
     </>
   );
 }
